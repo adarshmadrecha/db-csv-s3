f39cfaf16c41af72fef9db3c32b7780de99ccbaa
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2020-12-20 21:19:00 +0530

    added bucketname and comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 async function db_csv_s3({
-  AWSCred, s3Prefix = '',
-  knex, tables = [],
+  S3, bucketname, s3Prefix = '',
+  Knex, tables = [],
   rows = 50000,
 }) {
-  // Do stuff
+  // Throw error if params not correct
+
+  // Loop tables
+  // Call DB function
+  // Call json2csv funtion
+  // Call s3 fucntion
 }
 
 module.export = { db_csv_s3 }
