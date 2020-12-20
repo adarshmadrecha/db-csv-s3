38ed4402c3a43e68f7231b8a6a38292f278284fb
yogesh.ms <kyogesh.ms@gmail.com>
2020-12-21 01:41:46 +0530

    looping tables array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { getDataFromDb } = './db'
+
 async function db_csv_s3({
   S3, bucketname, s3Prefix = '',
   Knex, tables = [],
@@ -6,9 +8,10 @@ async function db_csv_s3({
   // Throw error if params not correct
 
   // Loop tables
-  // Call DB function
-  // Call json2csv funtion
-  // Call s3 fucntion
+  tables.forEach(t => {
+    // Call DB function
+    await getDataFromDb({ Knex, tablename: t.table, rows, where: t.where, orderby: t.orderby, S3, bucketname, s3Prefix = '' })
+  });
 }
 
 module.export = { db_csv_s3 }
