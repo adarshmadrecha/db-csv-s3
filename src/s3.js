3e765b95628ed834e2e652d71e4571e937243429
yogesh.ms <kyogesh.ms@gmail.com>
2020-12-21 01:42:07 +0530

    uploading to s3

--- src/s3.js
+++ src/s3.js
@@ -1,5 +1,17 @@
-async function uploadFileToS3({ AWSCred, filename }) {
+async function uploadFileToS3({ S3, bucketname, filename, csvData }) {
 
+  // Setting up S3 upload parameters
+  const s3UploadParams = {
+    Bucket: bucketname,
+    Key: filename,
+    Body: csvData,
+  }
+
+  // Uploading files to the bucket
+  await S3.upload(s3UploadParams, (err, data) => {
+    if (err) throw Error(err)
+    console.log(data)
+  })
 }
 
 module.export = { uploadFileToS3 }
