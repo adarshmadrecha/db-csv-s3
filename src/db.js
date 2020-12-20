ed0ce1bfd9ca36eb8301c3fc36ca19a158ab31c0
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2020-12-20 21:18:47 +0530

    added comments

--- src/db.js
+++ src/db.js
@@ -1,5 +1,11 @@
-async function getDataFromDb({ knex, tablename, rows }) {
+async function getDataFromDb({ Knex, tablename, rows, where, orderby }) {
 
+  // Get Data from DB & return
+
+}
+
+function json2CSV(data) {
+  // covert and return
 }
 
 module.export = { getDataFromDb }
