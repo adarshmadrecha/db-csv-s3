8e72c431217f467e252cd967d955f2a360110bf2
yogesh.ms <kyogesh.ms@gmail.com>
2020-12-21 09:46:57 +0530

    shifted all the functions to index file

--- src/db.js
+++ src/db.js
@@ -1,35 +1,21 @@
 import { unparse } from 'papaparse';
-import dayjs from 'dayjs';
-import { uploadFileToS3 } from './s3';
 
-async function getDataFromDb({ Knex, tablename, rows, where, orderby, S3, bucketname, s3Prefix = '' }) {
-  try {
-    // Get Data from DB & return
-    const tableData = await Knex(tablename)
-      .select()
-      .where(where)
-      .orderBy(orderby)
+async function getDataFromDb({ Knex, tablename, rows, pageno, where, orderby }) {
 
-    let i = 0;
-    let page = 1
-    // Divide the tableData into chunks
-    while (i < tableData.length) {
-      const data = arr.slice(i, i += rows)
-      // Call json2csv funtion
-      const csvData = json2CSV(data)
-      // Get Current Date time
-      const todayDate = dayjs().format('YYYY-MM-DD HH:mm:ss')
-      // Generate file name
-      const fileName = s3Prefix ? `${s3Prefix}/${tablename}_${todayDate}_${page}.csv` : `${tablename}_${todayDate}_${page}.csv`
-      // Call s3 fucntion
-      await uploadFileToS3({ S3, bucketname, fileName, csvData })
-      // Increment Page no
-      page += 1;
-    }
-  } catch (error) {
+  // Get Data from DB & return
+  const sql = Knex(tablename)
+    .select()
+    .limit(rows)
+    .offset((pageno - 1) * rows);
 
+  if (where) {
+    sql.where(where)
+  }
+  if (orderby) {
+    sql.orderBy(orderby)
   }
 
+  return await sql;
 }
 
 function json2CSV(data) {
@@ -37,4 +23,4 @@ function json2CSV(data) {
   return unparse(JSON.stringify(data))
 }
 
-module.export = { getDataFromDb }
+module.export = { getDataFromDb, json2CSV }
