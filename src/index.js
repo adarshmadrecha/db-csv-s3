8898bebde84fd7704690da3813055200c0f6a1a3
yogesh.ms <kyogesh.ms@gmail.com>
2020-12-21 09:47:18 +0530

    optimized db_csv_s3 function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const { getDataFromDb } = './db'
+import dayjs from 'dayjs';
+import { uploadFileToS3 } from './s3';
+import { json2CSV } from './db';
 
 async function db_csv_s3({
   S3, bucketname, s3Prefix = '',
@@ -6,11 +9,32 @@ async function db_csv_s3({
   rows = 50000,
 }) {
   // Throw error if params not correct
+  if (!S3 || !Knex || !bucketname) {
+    throw Error('Params are missing')
+  }
 
+  // Get Current Date time
+  const todayDate = dayjs().format('YYYY-MM-DD HH:mm')
+  s3Prefix = s3Prefix ? `${s3Prefix}/` : ''
   // Loop tables
   tables.forEach(t => {
-    // Call DB function
-    await getDataFromDb({ Knex, tablename: t.table, rows, where: t.where, orderby: t.orderby, S3, bucketname, s3Prefix = '' })
+    let pageno = 1
+    while (pageno < 10000) {
+      // Call DB function
+      const data = await getDataFromDb({ Knex, tablename: t.table, rows, pageno, where: t.where, orderby: t.orderby })
+      if (data.length == 0) {
+        break;
+      }
+      // convert json to CSV
+      const csvData = json2CSV(data)
+      // Generate file name
+      const fileName = `${s3Prefix}${todayDate}/${tablename}_${page}.csv`
+      // Call s3 fucntion
+      await uploadFileToS3({ S3, bucketname, fileName, csvData })
+
+      pageno += 1;
+
+    }
   });
 }
 
