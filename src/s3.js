ee8f06a412028bf39ffcc9080c44ab5593955ca5
yogesh.ms <kyogesh.ms@gmail.com>
2020-12-23 21:49:57 +0530

    removed call back

--- src/s3.js
+++ src/s3.js
@@ -8,10 +8,7 @@ async function uploadFileToS3({ S3, bucketname, filename, csvData }) {
   }
 
   // Uploading files to the bucket
-  await S3.upload(s3UploadParams, (err, data) => {
-    if (err) console.log(err)
-    console.log(data)
-  })
+  await S3.upload(s3UploadParams).promise();
 }
 
 module.exports = { uploadFileToS3 }
